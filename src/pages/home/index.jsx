ade6b540de737e629649e8383e8f5404371adab2
JV-Albuquerque-S <alb.albqrq@gmail.com>
2023-03-21 16:50:20 -0300

    initial configuration

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,3 @@
+//Lorem ipsum dolor sit amet consectetur adipisicing elit.
+//Quasi enim voluptatem ea neque tempore obcaecati aut corporis dolor.
+//Accusantium, aperiam labore nam nemo molestias ut perspiciatis deleniti iure aliquam repellendus?
